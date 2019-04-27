2c8b10e8668c44f395772363bf680718da0228d5
LinkDaddy <49046578+LinkDaddy@users.noreply.github.com>
2019-04-27 23:18:30 +0800

    Update models.py
    fix unique max_length>255

--- memories/blog/models.py
+++ memories/blog/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
         ('0', '草稿'),
         ('1', '公开'),
     )
-    title = models.CharField(verbose_name="文章标题", max_length=256, unique=True)
+    title = models.CharField(verbose_name="文章标题", max_length=128, unique=True)
     content = models.TextField(verbose_name="正文")
     status = models.CharField(verbose_name='状态', max_length=1, choices=_STATUS_CHOICES, default='0')
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
